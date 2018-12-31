7824425b5ac9f5939c391308c78cd212351d86c6
Ezzat Qupty <ezzat.qupty12345@gmail.com>
2018-12-31 12:32:04 +0200

    Initial Commit

---
+++ mtmMath/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mtmMath
+//
+//  Created by Ezzat Qupty on 31/12/2018.
+//  Copyright © 2018 Ezzat Qupty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
