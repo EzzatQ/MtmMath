ff23a64b40062df66ee0d17f185c2b1e9ffbb533
Ezzat Qupty <ezzat.qupty12345@gmail.com>
2019-01-07 15:08:04 +0200

    Complex Done

--- mtmMath/Complex.cpp
+++ mtmMath/Complex.cpp
@@ -1,9 +1,26 @@
-//
-//  Complex.cpp
-//  mtmMath
-//
-//  Created by Ezzat Qupty on 07/01/2019.
-//  Copyright © 2019 Ezzat Qupty. All rights reserved.
-//
-
 #include "Complex.hpp"
+
+Complex& Complex::operator+=(const Complex& c){
+    re += c.re;
+    im += c.im;
+    return *this;
+}
+
+Complex& Complex::operator-=(const Complex& c){
+    return Complex::operator+=(-c);
+}
+
+Complex operator+(const Complex& a, const Complex& b){
+    Complex c = a;
+    return c +=b;
+}
+
+Complex operator-(const Complex& a, const Complex& b){
+    Complex c = a;
+    return c -=b;
+}
+
+bool Complex::operator==(const Complex& c) const {
+    return re == c.re && im == c.im;
+}
+
--- mtmMath/Complex.hpp
+++ mtmMath/Complex.hpp
@@ -1,14 +1,15 @@
-//
-//  Complex.hpp
-//  mtmMath
-//
-//  Created by Ezzat Qupty on 07/01/2019.
-//  Copyright © 2019 Ezzat Qupty. All rights reserved.
-//
-
 #ifndef Complex_hpp
 #define Complex_hpp
 
 #include <stdio.h>
 
+class Complex {
+    double re, im;
+public:
+    Complex(double r = 0, double i = 0): re(r), im(i){}
+    Complex& operator+=(const Complex& c);
+    Complex& operator-=(const Complex& c);
+    Complex operator-() const {return Complex(-re, -im);}
+    bool operator==(const Complex& c) const;
+};
 #endif /* Complex_hpp */
