4e6179da140e7ad9b7787c950c5672566d762d0b
Ezzat Qupty <ezzat.qupty12345@gmail.com>
2019-01-07 13:39:59 +0200

    Added cpp files

---
+++ mtmMath/Complex.hpp
@@ -0,0 +1,14 @@
+//
+//  Complex.hpp
+//  mtmMath
+//
+//  Created by Ezzat Qupty on 07/01/2019.
+//  Copyright © 2019 Ezzat Qupty. All rights reserved.
+//
+
+#ifndef Complex_hpp
+#define Complex_hpp
+
+#include <stdio.h>
+
+#endif /* Complex_hpp */
---
+++ mtmMath/MtmMatSq.cpp
@@ -0,0 +1,9 @@
+//
+//  MtmMatSq.cpp
+//  mtmMath
+//
+//  Created by Ezzat Qupty on 07/01/2019.
+//  Copyright © 2019 Ezzat Qupty. All rights reserved.
+//
+
+#include <stdio.h>
