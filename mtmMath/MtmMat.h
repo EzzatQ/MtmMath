998fd67d45df19402da77e97988cd7ce38ab13d2
Ezzat Qupty <ezzat.qupty12345@gmail.com>
2019-01-14 15:34:19 +0200

    d

--- mtmMath/MtmMat.h
+++ mtmMath/MtmMat.h
@@ -15,13 +15,19 @@ namespace MtmMath {
 
     template <typename T>
     class MtmMat {
-        MtmVec<MtmVec<T>> matrix;
+    public:
+        //protected:
+        MtmVec<MtmVec<T> > matrix;
         Dimensions dim;
     public:
         /*
          * Matrix constructor, dim_t is the dimension of the matrix and val is the initial value for the matrix elements
          */
         MtmMat(Dimensions dim_t, const T& val=T());
+        
+        MtmMat(){
+            MtmMat(Dimensions(1, 1));
+        }
         
         ~MtmMat(); //
         
@@ -66,8 +72,8 @@ namespace MtmMath {
                 throw MtmExceptions::AccessIllegalElement();
             }
             MtmVec<T> a(row);
-            for (int j = 0; j < row; j++) {
-                a.vect[j]= matrix[j][i];
+            for(int j = 0; j < row; j++) {
+                a.vect[j] = matrix[j][i];
             }
             return a;
         }
@@ -80,6 +86,195 @@ namespace MtmMath {
         }
 
     };
+    
+    //the constructor
+    template <class T>
+    MtmMat<T>::MtmMat(Dimensions dim_t, const T& val):
+    matrix(dim_t.getCol(),MtmVec<T>(dim_t.getRow(),val)) , dim(dim_t){
+        matrix.transpose();
+    }
+    
+    //the copy constructor
+    template <class T>
+    MtmMat<T>::MtmMat(const MtmMat& mat): matrix(MtmVec<MtmVec<T> >(1,T())){
+        matrix = mat.matrix;
+        dim = mat.dim;
+    }
+    
+    
+    //the destructor
+    template <class T>
+    MtmMat<T>::~MtmMat<T>(){}
+    
+    
+    //the assigning operator
+    template <class T>
+    MtmMat<T>& MtmMat<T>::operator=(const MtmMat& mat){
+        if(this == &mat){
+            return *this;
+        }
+        matrix = mat.matrix;
+        dim = mat.dim;
+        return *this;
+    }
+    
+    
+    //who said func works on columns?
+    template <class T>
+    template <typename Func>
+    MtmVec<T> MtmMat<T>::matFunc(Func& f) const{
+        MtmVec<T> output(dim.getCol());
+        for(int i = 0; i < dim.getCol(); i++){
+            output[i] = matrix[i].vecFunc(f);
+        }
+        return output;
+    }
+    
+    //what should it do exactly?
+    //switch the places for col and row as arguments
+    template <class T>
+    void MtmMat<T>::resize(Dimensions dim, const T& val){
+        size_t newCol = dim.getCol();
+        size_t newRow = dim.getRow();
+        matrix.resize(Dimensions(1, newCol), val);
+        size_t colSize = matrix.size();
+        for(int i = 0; i < colSize; i++){
+            matrix[i].resize(Dimensions(newRow,1), val);
+        }
+    }
+    
+    
+    //
+    template <class T>
+    void MtmMat<T>::reshape(Dimensions newDim){
+        size_t newCol = newDim.getCol(), newRow = newDim.getRow();
+        size_t col = dim.getCol(), row = dim.getCol();
+        if(newCol * newRow != col * row){
+            throw MtmExceptions::DimensionMismatch();
+        }
+        T* tempArr = new T[col*row];
+        for(int i = 0; i < col; i++){
+            for(int j = 0; j < row; j++){
+                tempArr[i * col + j] = matrix[i][j];
+            }
+        }
+        resize(newDim);
+        for(int i = 0; i < newCol; i++){
+            for(int j = 0; j < newRow; j++){
+                matrix[i][j] = tempArr[i * newCol + j];
+            }
+        }
+        delete[] tempArr;
+    }
+    
+    
+    //nice
+    template<class T>
+    void MtmMat<T>::transpose(){
+        size_t col = dim.getCol(), row = dim.getRow();
+        dim.transpose();
+        MtmMat<T> temp(*this);
+        reshape(Dimensions(col,row));
+        for(int i = 0; i < col; i++){
+            for(int j = 0; j < row; j++){
+                matrix[j][i] = temp[i][j];
+            }
+        }
+    }
+    
+    template <class T>
+    MtmMat<T>& MtmMat<T>::operator+=(const MtmMat& mat){
+        matrix = matrix + mat;
+        return *this;
+    }
+    
+    template <class T>
+    MtmMat<T>& MtmMat<T>::operator-=(const MtmMat& mat){
+        matrix = matrix - mat;
+        return *this;
+    }
+    
+    template <class T>
+    MtmMat<T>& MtmMat<T>::operator+=(const T& s){
+        MtmMat<T> mat(dim,s);
+        return matrix += mat;
+    }
+    
+    template <class T>
+    MtmMat<T>& MtmMat<T>::operator-=(const T& s){
+        MtmMat<T> mat(dim,s);
+        return matrix -= mat;
+    }
+    
+    template <class T>
+    MtmMat<T>& MtmMat<T>::operator*=(const MtmMat& mat){
+        size_t rows = mat.dim.getRow(), cols = mat.dim.getCol(), thisRows = dim.getRow();///////////// get row/col for mat
+        if(rows != dim.getCol()){
+            throw MtmExceptions::DimensionMismatch();
+        }
+        MtmMat<T> result(Dimensions(thisRows,cols),T());
+        for (unsigned int i = 0; i < thisRows; i++) {
+            for (unsigned int j = 0; j < cols; j++) {
+                T sum = 0;//?
+                for (unsigned int m=0; m < rows; m++) {
+                    sum+= matrix[i][m] * mat.matrix[m][j];
+                }
+                result[i][j] = sum;
+            }
+        }
+        return &result;
+    }
+    
+    template <class T>
+    MtmMat<T>& MtmMat<T>::operator*=(const T& s){
+        size_t cols = dim.getCol();
+        MtmMat<T> newMat(Dimensions(cols,cols),0); //is it legal?
+        for (int i = 0; i < cols; i++) {
+            newMat[i][i] = s;
+        }
+        return this*=newMat;
+    }
+    template <class T>
+    MtmMat<T>& MtmMat<T>::operator*=(const MtmVec<T>& vec){
+        Dimensions d(vec.size(),1);
+        if(!vec.column)
+            d.transpose();
+        MtmMat<T> newMat(d,T());
+        int m = 0;
+        for (int j = 0; j < d.getCol(); j++) {
+            for (int i = 0; i < d.getRow(); i++) {
+                newMat[i][j] = vec[m];
+                m++;
+            }
+        }
+        return this*=newMat;
+    }
+    
+    
+    ///////things that need to be templated for all functions
+    
+    
+    
+    
+    template <class T>
+    T& operator+(T a,T b){
+        if(a != nullptr)
+            return a += b;
+    }
+    
+    template <class T>
+    T& operator-(T a,T b){
+        if(a != nullptr)
+            return a -= b;
+    }
+    
+    template <class T, class S>
+    MtmMat<S>& operator*(MtmMat<S> a,T b){
+        if(a != nullptr)
+            return a *= b;
+    }
+    
+    
 }
 
 
